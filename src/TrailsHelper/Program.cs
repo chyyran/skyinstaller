eed8d656139ccf89d80a3d392cb6cb94e5e1ddd6
chyyran <ronny@ronnychan.ca>
2022-06-20 13:59:01 -0400

    ui: prevent multiple instances

--- src/TrailsHelper/Program.cs
+++ src/TrailsHelper/Program.cs
@@ -2,19 +2,30 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.ReactiveUI;
 using System;
+using System.Reflection;
+using System.Threading;
 
 namespace TrailsHelper
 {
     internal class Program
     {
+        private static Mutex? singleInstanceMutex = null;
+
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args)
-            ;
+        public static void Main(string[] args)
+        {
+            singleInstanceMutex = new Mutex(true, Assembly.GetExecutingAssembly().GetName().Name, out bool isNew);
+            if (!isNew) 
+            {
+                return;
+            }
 
+            BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+        }
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
